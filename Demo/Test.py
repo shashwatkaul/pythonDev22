a941237ec862340332b548938c62b82924635d3e
shashwatkaul <shashwatkaul14@gmail.com>
2020-05-10 20:33:47 +0530

    Add files via upload

---
+++ Demo/Test.py
@@ -0,0 +1,14 @@
+'''
+Created on May 10, 2020
+
+@author: hp1
+'''
+#from selenium import webdriver
+#from selenium.webdriver.common.keys import Keys
+#print("test case started")
+#driver = webdriver.Chrome()  
+#driver.maximize_window()  
+#driver.get("https://www.gmail.com")
+#driver.close()
+
+class Test():
